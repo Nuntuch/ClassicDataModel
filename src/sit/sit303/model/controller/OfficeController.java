7b76fb3e9140e156f7249fa6fbcdc6893490d8ed
Nuntuch <nuntuch13589@gmail.com>
2018-09-12 16:14:10 +0700

    ADD Office.java

---
+++ src/sit/sit303/model/controller/OfficeController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.sit303.model.controller;
+
+import java.util.List;
+import sit.sit303.model.Office;
+
+/**
+ *
+ * @author Student Lab
+ */
+public class OfficeController {
+    public  Office find(String officeCode){
+    
+        return null;
+    }
+    public List<Office> getAll(){
+    
+        return null;
+    }
+}
