2ec934ae8b86ba43e2fdcb6224b10436b11a610b
Nuntuch <nuntuch13589@gmail.com>
2018-09-12 14:45:47 +0700

    ADD TestJDBC.java

---
+++ src/sit/int303/test/TestJDBC.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.int303.test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Student Lab
+ */
+public class TestJDBC {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDataSource");//ADD DriveDB
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+    }
+}
