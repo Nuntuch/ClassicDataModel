f0deacbc8ef568c6b1a88f6bbbb0b9853df6217e
Nuntuch <nuntuch13589@gmail.com>
2018-09-12 16:22:09 +0700

    Edit ConnectionFactory.java

--- src/sit/sit303/model/controller/ConnectionFactory.java
+++ src/sit/sit303/model/controller/ConnectionFactory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sit.sit303.model;
+package sit.sit303.model.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/sit/sit303/model/controller/OfficeController.java
+++ src/sit/sit303/model/controller/OfficeController.java
@@ -5,7 +5,12 @@
  */
 package sit.sit303.model.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import sit.sit303.model.Office;
 
 /**
@@ -13,8 +18,15 @@ import sit.sit303.model.Office;
  * @author Student Lab
  */
 public class OfficeController {
+    private final static String FIND ="SELECT * FROM office o WHERE o.officecode = ?";
+    private final static String GET_ALL ="SELECT * FROM office";
     public  Office find(String officeCode){
-    
+    Connection conn = ConnectionFactory.getConnnection();
+        try {
+            PreparedStatement pstm = conn.prepareStatement(FIND);
+        } catch (SQLException ex) {
+            Logger.getLogger(OfficeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
     public List<Office> getAll(){
