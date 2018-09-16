d55f86b22be591fbb345e6f2f34cb3b2a5a92b14
Unknown <nuntuch13589@gmail.com>
2018-09-16 21:08:10 +0700

    ADD Dice.java AND TestDice.java

---
+++ test/sit/int303/test/TestDice.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.int303.test;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import sit.sit303.model.Dice;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ */
+public class TestDice {
+    
+    public TestDice() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.         
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+@Test
+public void ลูกเต๋าที่เกิดใหม่หน้าที่ออกมาต้องไปเป็นศูนย์(){
+     Dice dice = new Dice();
+
+}
+
+}
